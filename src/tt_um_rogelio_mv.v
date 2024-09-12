23808367b145c684befe0976b504aa9619f947d1
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-11 20:48:52 -0700

    Update tt_um_rogelio_mv.v

--- src/tt_um_rogelio_mv.v
+++ src/tt_um_rogelio_mv.v
@@ -54,38 +54,6 @@ module tt_um_rogelio_mv #( parameter MAX_COUNT = 24'd10_000_000 ) (
                 second_counter <= second_counter + 1'b1;
         end
     end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
-
-    module seg7 (
-    input wire [3:0] counter,
-    output reg [6:0] segments
-);
-
-    always @(*) begin
-        case(counter)
-            0: segments = 7'b0111110; //U
-            1: segments = 7'b1110111; //A
-            2: segments = 7'b1111100; //B
-            3: segments = 7'b0111001; //C
-            4: segments = 7'b1000000; //-
-            5: segments = 7'b1111001; //E
-            6: segments = 7'b0111000; //L 
-            7: segments = 7'b1111001; //E
-            8: segments = 7'b0111001; //C
-            9: segments = 7'b0110001; //T
-            10: segments = 7'b1010000; //R
-            11: segments = 7'b0111111; //O
-            12: segments = 7'b1010100; //N
-            13: segments = 7'b0110000; //I
-            14: segments = 7'b0111001; //C
-            15: segments = 7'b1110111; //A
-            default:    
-                segments = 7'b0000000;
-        endcase
-    end
-
     
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, 1'b0};

ec264321df91c1f4396a41d8fab33a369d98bc2b
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-11 20:52:07 -0700

    Update tt_um_rogelio_mv.v

--- src/tt_um_rogelio_mv.v
+++ src/tt_um_rogelio_mv.v
@@ -16,46 +16,9 @@ module tt_um_rogelio_mv #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    wire reset = ! rst_n;
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
-
-     // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
-
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
-
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
-
-    always @(posedge clk) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == MAX_COUNT) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 15
-                if (digit == 16)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
+    
     
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, 1'b0};
+    wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
